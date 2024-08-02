8f699e7fc02ffa4b31167741ad16f3af8de3f639
hng011 <hansnaufal.3040@gmail.com>
2024-08-02 22:44:31 +0700

    fix: updated ALLOWED_HOST on settings.py

--- django_face_detection/settings.py
+++ django_face_detection/settings.py
@@ -26,8 +26,7 @@ SECRET_KEY = os.environ.get('NGAK')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = int(os.environ.get('DEBUG'))
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ["*"]
 
 # Application definition
 
