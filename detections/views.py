6c0594ed4af527db6d6ab9740ffc619f6df6a372
hng011 <hansnaufal.3040@gmail.com>
2024-08-03 04:04:57 +0700

    added a comment on detections/views.py

--- detections/views.py
+++ detections/views.py
@@ -3,11 +3,14 @@ from django.http import StreamingHttpResponse
 from .camera import Camera
 
 # Create your views here.
-def gen(cam):
+def generator(cam):
+    """_summary_
+    Continuously fetch frames from a cam object 
+    """
     while True:
         f = cam.get_frame()
         yield (b"--frame\r\n"
                b"Content-Type: image/jpeg\r\n\r\n" + f + b"\r\n\r\n")
         
 def video_feed(req):
-    return StreamingHttpResponse(gen(Camera()), content_type="multipart/x-mixed-replace; boundary=frame")
+    return StreamingHttpResponse(generator(Camera()), content_type="multipart/x-mixed-replace; boundary=frame")
